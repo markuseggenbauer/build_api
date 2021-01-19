4e1f4ce101a36fb4c1969730a015d236be03b493
Eggenbauer Markus <markus.eggenbauer@gmail.com>
2021-01-19 21:27:39 +0000

    improved mocking robustness (using global tracing variables)

--- cmake/lib/src/me_mock.cmake
+++ cmake/lib/src/me_mock.cmake
@@ -1,34 +1,28 @@
 include_guard()
 
-macro(me_mock_trace function_name)
+function(me_mock_trace function_name)
   string(REPLACE ";" "|" _parameters "${ARGN}")
-  list(APPEND ME_MOCK_TRACING_RECORD "${function_name}(${_parameters})")
-  set(ME_MOCK_TRACING_RECORD
-      ${ME_MOCK_TRACING_RECORD}
-      PARENT_SCOPE)
+  set(ENV{ME_MOCK_TRACING_RECORD}
+      "$ENV{ME_MOCK_TRACING_RECORD};${function_name}(${_parameters})")
   message(TRACE "  !: ${function_name}(${_parameters})")
-endmacro()
+endfunction()
 
 function(me_mock_expect function_name)
   string(REPLACE ";" "|" _parameters "${ARGN}")
-  list(APPEND ME_MOCK_TRACING_EXPECTATION "${function_name}(${_parameters})")
-  set(ME_MOCK_TRACING_EXPECTATION
-      ${ME_MOCK_TRACING_EXPECTATION}
-      PARENT_SCOPE)
+  set(ENV{ME_MOCK_TRACING_EXPECTATION}
+      "$ENV{ME_MOCK_TRACING_EXPECTATION};${function_name}(${_parameters})")
   message(TRACE "  ?: ${function_name}(${_parameters})")
 endfunction()
 
 function(me_mock_reset)
-  set(ME_MOCK_TRACING_RECORD
-      ""
-      PARENT_SCOPE)
-  set(ME_MOCK_TRACING_EXPECTATION
-      ""
-      PARENT_SCOPE)
+  set(ENV{ME_MOCK_TRACING_RECORD})
+  set(ENV{ME_MOCK_TRACING_EXPECTATION})
 endfunction()
 
 function(me_mock_check_expectations)
-  if(ME_MOCK_TRACING_RECORD STREQUAL ME_MOCK_TRACING_EXPECTATION)
+  set(TRACING_RECORD $ENV{ME_MOCK_TRACING_RECORD})
+  set(TRACING_EXPECTATION $ENV{ME_MOCK_TRACING_EXPECTATION})
+  if(TRACING_RECORD STREQUAL TRACING_EXPECTATION)
     message(STATUS "  Success")
   else()
     message(FATAL_ERROR "  Fail")
