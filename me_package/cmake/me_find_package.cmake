fdefb049f7786b20da2deb514677184d231833fa
Eggenbauer Markus <markus.eggenbauer@gmail.com>
2021-03-06 09:56:44 +0000

    make package manager support optional, fix bugs

--- me_package/cmake/me_find_package.cmake
+++ me_package/cmake/me_find_package.cmake
@@ -16,38 +16,44 @@ function(
     message(DEBUG "me_package_internal_get_package_metadata(${package_name})")
 
     string(TOUPPER ${package_name} package_name_upper)
-    set(
-        ${out_repo_url}
-        "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SOURCE}"
-        PARENT_SCOPE
-    )
-    string(REGEX REPLACE ".*\/" "" repo_name ${ME_PACKAGE_METADATA_${package_name_upper}_SOURCE})
-    string(REGEX REPLACE "\\..*" "" repo_name ${repo_name})
-    set(
-        ${out_repo_name}
-        "${repo_name}"
-        PARENT_SCOPE
-    )
-    set(
-        ${out_sub_dir}
-        "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR}"
-        PARENT_SCOPE
-    )
-    set(
-        ${out_version}
-        "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_VERSION}"
-        PARENT_SCOPE
-    )
+    if($CACHE{ME_PACKAGE_METADATA_${package_name_upper}_EXISTS})
 
-    message(DEBUG "me_package_internal_get_package_metadata(${package_name}):")
-    message(DEBUG "  out_repo_url=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SOURCE}\"")
-    message(DEBUG "  out_repo_name=\"${repo_name}\"")
-    message(DEBUG "  out_sub_dir=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR}\"")
-    message(DEBUG "  out_version=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_VERSION}\"")
+        set(
+            ${out_repo_url}
+            "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SOURCE}"
+            PARENT_SCOPE
+        )
+        string(
+            REGEX REPLACE ".*\/" "" repo_name ${ME_PACKAGE_METADATA_${package_name_upper}_SOURCE}
+        )
+        string(REGEX REPLACE "\\..*" "" repo_name ${repo_name})
+        set(
+            ${out_repo_name}
+            "${repo_name}"
+            PARENT_SCOPE
+        )
+        set(
+            ${out_sub_dir}
+            "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR}"
+            PARENT_SCOPE
+        )
+        set(
+            ${out_version}
+            "$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_VERSION}"
+            PARENT_SCOPE
+        )
+
+        message(DEBUG "me_package_internal_get_package_metadata(${package_name}):")
+        message(DEBUG "  out_repo_url=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SOURCE}\"")
+        message(DEBUG "  out_repo_name=\"${repo_name}\"")
+        message(DEBUG "  out_sub_dir=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR}\"")
+        message(DEBUG "  out_version=\"$CACHE{ME_PACKAGE_METADATA_${package_name_upper}_VERSION}\"")
+    endif()
 endfunction()
 
 function(me_package_internal_find_package_local package_name out_result)
     message(DEBUG "me_package_internal_find_package_local(${package_name})")
+    set(result FALSE)
     cmake_parse_arguments(
         "PARAMETER"
         "QUIET;REQUIRED"
@@ -62,30 +68,30 @@ function(me_package_internal_find_package_local package_name out_result)
         sub_dir
         version
     )
-
-    set(local_package_dir "$CACHE{ME_PACKAGE_LOCAL_PACKAGE_SOURCE_DIR}/${repo_name}/${sub_dir}")
-    set(result FALSE)
-    if(EXISTS "${local_package_dir}/CMakeLists.txt")
-        message(STATUS "Found local package: ${package_name} in ${local_package_dir}")
-        add_subdirectory("${package_dir}" ${package_name})
-        if(PARAMETER_COMPONENTS OR PARAMETER_OPTIONAL_COMPONENTS)
-            foreach(component IN ITEMS ${PARAMETER_COMPONENTS})
-                add_library(${package_name}::${component} ALIAS ${component})
-            endforeach()
-            foreach(component IN ITEMS ${PARAMETER_OPTIONAL_COMPONENTS})
-                if(TARGET ${component})
+    if(version)
+        set(local_package_dir "$CACHE{ME_PACKAGE_LOCAL_PACKAGE_SOURCE_DIR}/${repo_name}/${sub_dir}")
+        if(EXISTS "${local_package_dir}/CMakeLists.txt")
+            message(STATUS "Found local package: ${package_name} in ${local_package_dir}")
+            add_subdirectory("${local_package_dir}" ${package_name})
+            if(PARAMETER_COMPONENTS OR PARAMETER_OPTIONAL_COMPONENTS)
+                foreach(component IN ITEMS ${PARAMETER_COMPONENTS})
                     add_library(${package_name}::${component} ALIAS ${component})
+                endforeach()
+                foreach(component IN ITEMS ${PARAMETER_OPTIONAL_COMPONENTS})
+                    if(TARGET ${component})
+                        add_library(${package_name}::${component} ALIAS ${component})
+                    endif()
+                endforeach()
+            else()
+                message(STATUS "Found local package component: ${package_name}::${package_name}")
+                if(TARGET ${package_name})
+                    add_library(${package_name}::${package_name} ALIAS ${package_name})
                 endif()
-            endforeach()
-        else()
-            message(STATUS "Found local package component: ${package_name}::${package_name}")
-            if(TARGET ${package_name})
-                add_library(${package_name}::${package_name} ALIAS ${package_name})
             endif()
-        endif()
-        set(result TRUE)
-        set(${out_result} TRUE)
+            set(result TRUE)
+            set(${out_result} TRUE)
 
+        endif()
     endif()
     message(DEBUG "me_package_internal_find_package_local(${package_name}): ")
     message(DEBUG "  out_result=\"${result}\"")
@@ -100,21 +106,23 @@ function(me_package_internal_add_package_source package_name)
         version
     )
 
-    if(NOT TARGET fetch_source.all)
-        add_custom_target(fetch_source.all)
-    endif()
+    if(version)
+        if(NOT TARGET fetch_source.all)
+            add_custom_target(fetch_source.all)
+        endif()
 
-    if(NOT TARGET "fetch_source.${repo_name}")
-        add_custom_target(
-            "fetch_source.${repo_name}"
-            COMMAND "${GIT_EXECUTABLE}" clone "${repo}" -b "${version}"
-                    "$CACHE{ME_PACKAGE_LOCAL_PACKAGE_SOURCE_DIR}/${repo_name}"
-        )
-    endif()
+        if(NOT TARGET "fetch_source.${repo_name}")
+            add_custom_target(
+                "fetch_source.${repo_name}"
+                COMMAND "${GIT_EXECUTABLE}" clone "${repo_url}" -b "${version}"
+                        "$CACHE{ME_PACKAGE_LOCAL_PACKAGE_SOURCE_DIR}/${repo_name}"
+            )
+        endif()
 
-    if(NOT TARGET "fetch_source.${package_name}")
-        add_custom_target("fetch_source.${package_name}" DEPENDS "fetch_source.${repo_name}")
-        add_dependencies(fetch_source.all "fetch_source.${package_name}")
+        if(NOT TARGET "fetch_source.${package_name}")
+            add_custom_target("fetch_source.${package_name}" DEPENDS "fetch_source.${repo_name}")
+            add_dependencies(fetch_source.all "fetch_source.${package_name}")
+        endif()
     endif()
 endfunction()
 
