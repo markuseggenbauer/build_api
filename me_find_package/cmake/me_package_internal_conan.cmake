b890a7a7a7c2e0d620a115080a5b8e8d7e2cee36
Eggenbauer Markus <markus.eggenbauer@gmail.com>
2021-03-17 07:06:46 +0000

    UPdate me_find_package to use conan scm attributes

--- me_find_package/cmake/me_package_internal_conan.cmake
+++ me_find_package/cmake/me_package_internal_conan.cmake
@@ -5,32 +5,6 @@ if(CONAN_CMD STREQUAL CONAN-NOTFOUND)
     message(FATAL_ERROR "Mandatory program 'conan' was not found.")
 endif()
 
-function(me_package_conan_json_get_value input key output)
-    string(FIND "${input}" "\"${key}\": [" find_result)
-    if(find_result EQUAL -1)
-        string(FIND "${input}" "\"${key}\": \"" find_result)
-        if(find_result EQUAL -1)
-
-        else()
-            string(REGEX REPLACE ".*\"${key}\": \"" "" tmp "${input}")
-            string(REGEX REPLACE "\".*" "" result ${tmp})
-        endif()
-    else()
-        string(REGEX REPLACE ".*\"${key}\": \\[" "" tmp "${input}")
-        string(REGEX REPLACE "\\].*" "," tmp "${tmp}")
-        string(REGEX MATCHALL "\"[^\"]*\"" tmp "${tmp}")
-        string(REPLACE "\"" "" result "${tmp}")
-    endif()
-
-    if(result)
-        set(
-            ${output}
-            "${result}"
-            PARENT_SCOPE
-        )
-    endif()
-endfunction()
-
 function(me_package_conan_get_required_packages file_path out_packageids)
     file(STRINGS "${file_path}" file_content)
     string(REGEX MATCH "\\[full_requires\\][^\\[]*" file_content "${file_content}")
@@ -43,64 +17,57 @@ function(me_package_conan_get_required_packages file_path out_packageids)
     )
 endfunction()
 
-function(me_package_conan_write_package_json package_id)
+function(me_package_conan_get_scm_attribute input key output)
+    string(REGEX MATCH "${key}: [^ \t\r\n]+" find_result "${input}")
+    if(NOT (find_result STREQUAL ""))
+        string(REGEX REPLACE "${key}: " "" value "${find_result}")
+        set(
+            ${output}
+            "${value}"
+            PARENT_SCOPE
+        )
+    endif()
+endfunction()
+
+function(me_package_conan_populate_package_metadata package_id)
     string(REGEX REPLACE "/.*" "" package_name "${package_id}")
     string(REGEX REPLACE ":.*" "" package_name_version "${package_id}")
-    string(TOLOWER ${package_name} package_name_lower)
-    execute_process(
-        COMMAND "${CONAN_CMD}" inspect ${package_name_version} --json
-                "${CMAKE_BINARY_DIR}/${package_name_lower}.config.json" OUTPUT_QUIET
-    )
-endfunction()
-
-function(me_package_conan_read_package_json package_name out_content)
-    string(REGEX REPLACE "/.*" "" package_name "${package_id}")
-    string(TOLOWER ${package_name} package_name_lower)
-    file(READ "${CMAKE_BINARY_DIR}/${package_name_lower}.config.json" file_content)
-    set(
-        ${out_content}
-        "${file_content}"
-        PARENT_SCOPE
-    )
-endfunction()
-
-function(me_package_conan_populate_package_metadate_from_json package_id _json_content)
-    string(REGEX REPLACE "/.*" "" package_name "${package_id}")
     string(TOUPPER ${package_name} package_name_upper)
+    execute_process(
+        COMMAND "${CONAN_CMD}" inspect "${package_name_version}" -a scm OUTPUT_VARIABLE _scm_data
+    )
+    me_package_conan_get_scm_attribute(${_scm_data} type _type)
+    if(_type STREQUAL "git")
+        me_package_conan_get_scm_attribute(${_scm_data} url _url)
+        me_package_conan_get_scm_attribute(${_scm_data} revision _revision)
+        me_package_conan_get_scm_attribute(${_scm_data} subfolder _subfolder)
 
-    me_package_conan_json_get_value("${_json_content}" version _version)
+        set(
+            ME_PACKAGE_METADATA_${package_name_upper}_EXISTS
+            TRUE
+            CACHE BOOL "Package ${package_name_upper} exists."
+        )
+        set(
+            ME_PACKAGE_METADATA_${package_name_upper}_VERSION
+            "${_revision}"
+            CACHE STRING "Package ${package_name_upper} version."
+        )
+        set(
+            ME_PACKAGE_METADATA_${package_name_upper}_SOURCE
+            "${_url}"
+            CACHE STRING "Package ${package_name_upper} source origin"
+        )
 
-    me_package_conan_json_get_value("${_json_content}" url _url)
-    list(LENGTH _url _url_size)
-    list(GET _url 0 _source)
+        if(NOT _subfolder)
+            set(_subfolder ".")
+        endif()
 
-    if(_url_size EQUAL 2)
-        list(GET _url 1 _sub_dir)
-    else()
-        set(_sub_dir ".")
+        set(
+            ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR
+            "${_subfolder}"
+            CACHE STRING "Package ${package_name_upper} source sub-directory"
+        )
     endif()
-
-    set(
-        ME_PACKAGE_METADATA_${package_name_upper}_EXISTS
-        TRUE
-        CACHE BOOL "Package ${package_name_upper} exists."
-    )
-    set(
-        ME_PACKAGE_METADATA_${package_name_upper}_VERSION
-        "${_version}"
-        CACHE STRING "Package ${package_name_upper} version."
-    )
-    set(
-        ME_PACKAGE_METADATA_${package_name_upper}_SOURCE
-        "${_source}"
-        CACHE STRING "Package ${package_name_upper} source origin"
-    )
-    set(
-        ME_PACKAGE_METADATA_${package_name_upper}_SUB_DIR
-        "${_sub_dir}"
-        CACHE STRING "Package ${package_name_upper} source sub-directory"
-    )
-
 endfunction()
 
 # me_package_<type> implementation
@@ -108,9 +75,7 @@ function(me_package_internal_init)
     if(EXISTS "${CMAKE_BINARY_DIR}/conaninfo.txt")
         me_package_conan_get_required_packages("${CMAKE_BINARY_DIR}/conaninfo.txt" packages)
         foreach(package_id IN ITEMS ${packages})
-            me_package_conan_write_package_json(${package_id})
-            me_package_conan_read_package_json(${package_id} json_content)
-            me_package_conan_populate_package_metadate_from_json(${package_id} "${json_content}")
+            me_package_conan_populate_package_metadata(${package_id})
         endforeach()
     endif()
 endfunction()
